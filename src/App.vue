e4a25af8eaa15a5377c93a7ff33c3a47d70c6961
ceelogre <sam.nishimwe@yahoo.com>
2020-01-16 18:33:46 +0200

    Boilerplate
    Setup initial project structure
    
    Add vue-router
    
    Implement a basic routing mechanism
    
    Update README
    
    Add instructions on how to run the app
    
    Replace stock images with project images for homepage and favicon
    
    Add Bootstrap-vue

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.svg">
+    <p>
+      <router-link to='/'>Home</router-link> || 
+      <router-link to='/page'>Work</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/randomComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Of course! Get to work
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'random'
+}
+</script>
+
+<style>
+
+</style>
